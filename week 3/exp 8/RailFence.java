68cbec54b776d1397e9b3b58bedd66fa3d873a72
YASWANTH BATTU <83590022+s-1-n-t-h@users.noreply.github.com>
2022-12-26 18:20:34 +0530

    converted println's to print and resolving errors

--- week 3/exp 8/RailFence.java
+++ week 3/exp 8/RailFence.java
@@ -3,18 +3,43 @@ import java.lang.Math;
 class RailFence {
 
     void encrypt(String text, int depth){
-        String splittedText[][] = new String[depth][];
+        int length = text.length();
+        int cols = Math.ceilDiv(length, depth);
+        int rows = depth;
+        String splittedText[][] = new String[rows][cols];
+        int count,i,j;
+        
+        // i points the index of text
+        // j points the column index
+        // (i%depth) points the row index
+        count = 0;j =0;
+        for(i=0;i<length;i++){
+            if(count == depth){
+                j = 0;
+                count = 0;
+            }
+            splittedText[i%2][j++] = Character.toString(text.charAt(i));
+            count++;
+        }
+
+        // print the 2D array
+        for (i = 0; i < length; i++) {
+            for(j=0;j < depth;j++){
+                System.out.println(splittedText[i][j]);
+            }
+        }
+
     }
     public static void main(String[] args){
 
-        System.out.println("\n\t\t\t\t\tRAIL_FENCE CIPHER TECHNIQUEt: ");
+        System.out.print("\n\t\t\t\t\tRAIL_FENCE CIPHER TECHNIQUEt: ");
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("\ntext: ");
+        System.out.print("\ntext: ");
         String text = sc.nextLine();
 
-        System.out.println("\nDepth: ");
+        System.out.print("\nDepth: ");
         int depth = sc.nextInt();
 
         new RailFence().encrypt(text, depth);
