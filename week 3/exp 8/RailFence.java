2c33a31ddaee6db49aa8da9eac18a62d2dc4a19d
YASWANTH BATTU <83590022+s-1-n-t-h@users.noreply.github.com>
2022-12-26 18:37:26 +0530

    fixed bug

--- week 3/exp 8/RailFence.java
+++ week 3/exp 8/RailFence.java
@@ -6,9 +6,7 @@ class RailFence {
         int length = text.length();
         int cols = Math.ceilDiv(length, depth);
         int rows = depth;
-        System.out.println(length);
-        System.out.println(rows);
-        System.out.println(cols);
+   
         char splittedText[][] = new char[rows][cols];
         int count,i,j;
         
@@ -18,10 +16,11 @@ class RailFence {
         count = 0;j =0;
         for(i=0;i<length;i++){
             if(count == depth){
-                j = 0;
-                count = 0;
+                j++; // shifiting j to point to next column
+                count = 0; // re initialize count to avoid replacing the data 
             }
-            splittedText[i%2][j++] = text.charAt(i);
+            splittedText[i%2][j] = text.charAt(i);  // 1. G O M R I G
+                                                    // 2. O D O N N *
             count++;
         }
 
@@ -30,17 +29,14 @@ class RailFence {
         }
 
         // print the 2D array
-        try{
-            for (i = 0; i < depth; i++) {
-                for(j=0;j < length;j++){
-                    System.out.print(splittedText[i][j]);
-                }
-                System.out.println();
+
+        for (i = 0; i < rows; i++) {
+            for(j=0;j < cols;j++){
+                System.out.print(splittedText[i][j]);
             }
+            System.out.println();
         }
-        catch(Exception e){
-            System.out.println(e.toString());
-        }
+        
     }
     public static void main(String[] args){
 
