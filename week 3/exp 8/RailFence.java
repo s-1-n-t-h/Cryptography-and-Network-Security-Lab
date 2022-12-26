7c4f32ab248d02d3c843a246be5e375d0d90ac9f
YASWANTH BATTU <83590022+s-1-n-t-h@users.noreply.github.com>
2022-12-26 17:56:03 +0530

    input variables for paramters, to generate cipher

--- week 3/exp 8/RailFence.java
+++ week 3/exp 8/RailFence.java
@@ -1,6 +1,19 @@
 import java.util.*;
 class RailFence {
     public static void main(String[] args){
-        String text = 
+
+        System.out.println("\n\t\t\t\t\tRAIL_FENCE CIPHER TECHNIQUEt: ");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("\ntext: ");
+        String text = sc.nextLine();
+        
+        System.out.println("\nDepth: ");
+        int depth = sc.nextInt();
+
+
+
+
     }
 }
