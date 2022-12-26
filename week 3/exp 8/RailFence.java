f0d5a6338e25fd51ef85f2fa841f5c3b56df2163
YASWANTH BATTU <83590022+s-1-n-t-h@users.noreply.github.com>
2022-12-26 18:02:11 +0530

    imported math lib for performing ceil() oprn

--- week 3/exp 8/RailFence.java
+++ week 3/exp 8/RailFence.java
@@ -1,5 +1,10 @@
 import java.util.*;
+import java.lang.Math;
 class RailFence {
+
+    void encrypt(String text, int depth){
+        String splittedText[][] = new String[depth][];
+    }
     public static void main(String[] args){
 
         System.out.println("\n\t\t\t\t\tRAIL_FENCE CIPHER TECHNIQUEt: ");
@@ -8,12 +13,10 @@ class RailFence {
 
         System.out.println("\ntext: ");
         String text = sc.nextLine();
-        
+
         System.out.println("\nDepth: ");
         int depth = sc.nextInt();
 
-
-
-
+        new RailFence().encrypt(text, depth);
     }
 }
