667e1b619d10cc7ec392da7a513a83994c49c5b8
YASWANTH BATTU <83590022+s-1-n-t-h@users.noreply.github.com>
2022-12-26 16:17:46 +0530

    Encryption Class impplemented

---
+++ week 3/exp 7/HillCipher.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class HillCipher extends Encrypt{
+    public static void main(String... args){
+        
+        System.out.print("Input Length of text: ");
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+
+        System.out.print("\nInput text: ");
+        String text = sc.nextLine();
+
+        System.out.print("\nInput key: ");
+        String key = sc.nextLine();
+
+        new HillCipher().encrypt(key,text,n);
+
+    }
+}
