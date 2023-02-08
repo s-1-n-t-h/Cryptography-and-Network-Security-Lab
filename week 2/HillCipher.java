b01f1a87772dc1b79ee3f7cd5887e1d952070345
YASWANTH BATTU <83590022+s-1-n-t-h@users.noreply.github.com>
2023-02-08 16:36:21 +0530

    3

---
+++ week 2/HillCipher.java
@@ -0,0 +1,63 @@
+// Java code to implement Hill Cipher
+class HillCipher {
+
+    // Following function encrypts the message
+    static void encrypt(int cipherMatrix[][],
+            int keyMatrix[][],
+            int messageVector[][]) {
+        int x, i, j;
+        for (i = 0; i < 3; i++) {
+            for (j = 0; j < 1; j++) {
+                cipherMatrix[i][j] = 0;
+
+                for (x = 0; x < 3; x++) {
+                    cipherMatrix[i][j] += keyMatrix[i][x] * messageVector[x][j];
+                }
+
+                cipherMatrix[i][j] = cipherMatrix[i][j] % 26;
+            }
+        }
+    }
+
+    // Function to implement Hill Cipher
+    static void HillCipher(String message,int n) {
+        // Get key matrix from the key string
+        int[][] keyMatrix = {{17,17,5},{21,18,21},{2,2,19}};
+        //getKeyMatrix(keyMatrix);
+
+        int[][] messageVector = new int[3][1];
+
+        // Generate vector for the message
+        for (int i = 0; i < 3; i++)
+            messageVector[i][0] = (message.charAt(i)) % 65;
+
+        int[][] cipherMatrix = new int[3][1];
+
+        // Following function generates
+        // the encrypted vector
+        encrypt(cipherMatrix, keyMatrix, messageVector);
+
+        String CipherText = "";
+
+        // Generate the encrypted text from
+        // the encrypted vector
+        for (int i = 0; i < 3; i++)
+            CipherText += (char) (cipherMatrix[i][0] + 65);
+
+        // Finally print the ciphertext
+        System.out.print(" Ciphertext:" + CipherText);
+    }
+
+    // Driver code
+    public static void main(String[] args) {
+        // Get the message to be encrypted
+        String message = "PAY";
+
+        // Get the key
+        //String key = "GYBNQKURP";
+
+        HillCipher(message,message.length());
+    }
+}
+
+// This code has been contributed by 29AjayKumar
